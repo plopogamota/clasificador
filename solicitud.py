452306a37869c5e220651ac44b830f2129ebf685
johnatan felipe alvarez arias <90900061+plopogamota@users.noreply.github.com>
2024-11-21 08:08:37 -0500

    Update solicitud.py

--- solicitud.py
+++ solicitud.py
@@ -5,7 +5,12 @@ url = "http://127.0.0.1:5000/predict"
 
 # Solicitud con un ejemplo de reseña
 data = {"review": "This app is amazing!"}
-response = requests.post(url, json=data)
+# Verifica si la reseña excede el límite de 500 caracteres
+if len(data['review']) > 200:
+    print("La reseña es demasiado larga. El máximo permitido es 200 caracteres.")
+else:
+    response = requests.post(url, json=data)
+    # Imprimir la respuesta
+    print(response.json())
 
-# Imprimir la respuesta
-print(response.json())
+
